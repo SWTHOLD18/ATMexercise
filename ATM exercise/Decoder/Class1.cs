3d0540ecff79f88b5ae71518ab10e00171500ce3
Oliver <oliver_mei@hotmail.com>
2019-03-12 13:59:32 +0100

    decodereadydata added to decoder and decoder subscribed to reciever following example given on blackboard

--- ATM exercise/Decoder/Class1.cs
+++ ATM exercise/Decoder/Class1.cs
@@ -1,8 +1,31 @@
 ﻿using System;
+using System.Globalization;
+using TransponderReceiver;
 
 namespace Decoder
 {
-    public class Class1
+    public class Decoder
     {
+        private ITransponderReceiver receiver;
+
+        public Decoder(ITransponderReceiver receiver)
+        {
+            this.receiver = receiver;
+            this.receiver.TransponderDataReady += decodeReadyData;
+        }
+
+        private void decodeReadyData(object sender, RawTransponderDataEventArgs arg)
+        {
+            foreach (var data in arg.TransponderData)
+            {
+                string[] plane = data.Split(';');
+                string tag = plane[0];
+                int xCord = Int32.Parse(plane[1]);
+                int yCord = Int32.Parse(plane[2]);
+                int alti = Int32.Parse(plane[3]);
+                string format = "yyyyMMddHHmmssfff";
+                DateTime time = DateTime.ParseExact(plane[4], format, CultureInfo.InvariantCulture);
+            }
+        }
     }
 }
