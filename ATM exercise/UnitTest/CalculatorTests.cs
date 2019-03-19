a79cef0db4a0beea8241d2f48a7dd99bbd28c8a0
florent417 <flole@live.dk>
2019-03-19 16:01:39 +0100

    Started implementing test

--- ATM exercise/UnitTest/CalculatorTests.cs
+++ ATM exercise/UnitTest/CalculatorTests.cs
@@ -1,14 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Decoder;
+using NUnit.Framework;
 
 namespace UnitTest
 {
-    [TestClass]
-    public class UnitTest1
+    [TestFixture]
+    public class CalculatorTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private Airplane _airplane1;
+        private Airplane _airplane2;
+        private Airplane _airplane3;
+        private Airplane _airplane4;
+
+        private List<Airplane> _airplaneList;
+
+        private Calculator _calculator;
+
+        [SetUp]
+        public void Setup()
         {
+            string timeStamp1 = "20151006213456001";
+            string timeStamp2 = "20151006213457001";
+            _airplane1 = new Airplane("ACR101", 40000, 40000, 8000, "20151006213456001");
         }
     }
 }
