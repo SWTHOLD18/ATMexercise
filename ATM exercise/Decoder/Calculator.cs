fe9aeaa68f172922788d2bf788afbbf4d1a53b64
florent417 <flole@live.dk>
2019-03-19 12:32:39 +0100

    Cleaned up in calculator

--- ATM exercise/Decoder/Calculator.cs
+++ ATM exercise/Decoder/Calculator.cs
@@ -32,30 +32,40 @@ namespace Decoder
 
             double angle = Math.Atan2(y,x);
            
-            //to make direction go clockwise we want the second quadrant to be negatve rather than positive
-            //we subtract PI to counteract Atan2's automatic +pi if x<0 and y>=0 and than subtract PI to make
-            //the degrees go negative clockwise
+            // To make direction go clockwise we want the second quadrant to be negatve rather than positive
+            // we subtract PI to counteract Atan2's automatic +pi if x<0 and y>=0 and than subtract PI to make
+            // the degrees go negative clockwise
             if(x < 0 && y>=0)
             {
                angle -= 2*Math.PI; 
             }
 
-            //Convert angle to degrees
+            // Convert angle to degrees
             var degrees = angle * 180/Math.PI;
 
-            //subtract 90degrees to make north 0 and invert to make the clockwise negative into clockwise positive
+            // Subtract 90degrees to make north 0 and invert to make the clockwise negative into clockwise positive
             var result = (degrees - 90) * -1;
 
-            //Return the current direction from the 2 inputs.
+            // Return the current direction from the 2 inputs.
             return result;
         }        
 
         public double CalculateSpeed(Airplane newAirplane)
         {
+            // Find oldAirplane with corresponding tag
             Airplane oldAirplane = oldaAirplaneList.Find(a => a.Tag == newAirplane.Tag);
+
+            // Check if airplane is in list
+            if (oldAirplane == null)
+            {
+                return 0;
+            }
+
+            // Differences in x and y coordinates
             double x_coordinate_difference = 0;
             double y_coordinate_difference = 0;
 
+            // Direction doesn't matter, but we need a positive value for the velocity
             if (newAirplane.X_coordinate > oldAirplane.X_coordinate)
             {
                 x_coordinate_difference = newAirplane.X_coordinate - oldAirplane.X_coordinate;
@@ -74,12 +84,14 @@ namespace Decoder
                 y_coordinate_difference = oldAirplane.Y_coordinate - newAirplane.Y_coordinate;
             }
 
-            TimeSpan tdif = newAirplane.Timestamp - oldAirplane.Timestamp;
-            var tdiff = (double) tdif.TotalSeconds;
+            // Timespan of the timestamps and then the time difference in double
+            TimeSpan tDif = newAirplane.Timestamp - oldAirplane.Timestamp;
+            var timeDifference = (double) tDif.TotalSeconds;
 
+            // Using pythagorean theorem to calculate the distance between the planes
             double distance = Math.Sqrt(Math.Pow(x_coordinate_difference, 2) + Math.Pow(y_coordinate_difference, 2));
 
-            return distance/tdiff;
+            return distance/timeDifference;
         }
     }
 }
