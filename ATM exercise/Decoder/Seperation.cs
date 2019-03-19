8343137970a99647a165b4387efefde81247fd61
ali <alielkaatib@hotmai.com>
2019-03-19 12:57:45 +0100

    seperation class

---
+++ ATM exercise/Decoder/Seperation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decoder
+{
+    class Seperation
+    {
+        public Airplane Airplane { get; set; }
+        public void newTrack(Airplane airplane)
+        {
+            Airplane = airplane;
+        }
+
+        public void checkWithOtherTracks(List<Airplane> airplaneList)
+        {
+            
+            for(int i=0; i < airplaneList.Count; i++)
+            {
+                for(int j = i + 1; j < airplaneList.Count; j++)
+                {
+                    
+                    if(airplaneList[i].X_coordinate + 5000 <= airplaneList[j].X_coordinate && airplaneList[i].Y_coordinate + 300 <= airplaneList[j].Y_coordinate)
+                    {
+                        var tag1 = airplaneList[i].Tag;
+                        var tag2 = airplaneList[j].Tag;
+                        var timestamp = airplaneList[i].Timestamp;
+                    }
+                }
+            }
+        }
+
+        public void updateCondition()
+        {
+
+        }
+    }
+}
